600b4a90c23e9fd3f6da617c026c01decf755a31
Justin Oswald <ozzy@demoncore.local>
2022-11-04 22:06:53 -0700

    Created Status enum

---
+++ src/application/Status.java
@@ -0,0 +1,11 @@
+package application;
+
+//Status enumerable used to figure out which linked list a pizza order should be in
+public enum Status {
+    NEW,        //0
+    READY,      //1
+    COOKING,    //2
+    PENDING,    //3
+    FINISHED,   //4
+    REJECT      //5
+}

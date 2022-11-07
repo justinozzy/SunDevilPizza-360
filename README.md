a1d6af05f342e59978a2a8e40423896aa0a7f73c
Justin Oswald <ozzy@demoncore.3521.dhcp.asu.edu>
2022-11-07 15:54:56 -0700

    Created test database and updated README dependencies

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # SunDevilPizza-360
 ASU's 360 Group Project
 
-### Java and JavaFX Versions
+## Dependencies
 
 ---
-Java: Oracle JDK version 18.0.2  
-JavaFX: SDK Version 18.0.2
+**Java:** Oracle JDK version 18.0.2 (*https://www.oracle.com/java/technologies/downloads/*)  
+**JavaFX:** SDK Version 18.0.2 (*https://gluonhq.com/products/javafx/*)  
+**SQLite:** SQLite JDBC 3.39.3.0 (*https://github.com/xerial/sqlite-jdbc*)
--- src/application/PizzaDatabase.java
+++ src/application/PizzaDatabase.java
@@ -1,24 +1,51 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class PizzaDatabase {
-    static final String db_url = "jdbc:sqlite:file://$PROJECT_DIR$/db/test.db";
+    static final String db_url = "jdbc:sqlite:db/test.db";
 
+    //TODO: Need to move void function to string value so that Linked Lists can be populated from a return
+    //TODO: Add parameters for the connection to determine which user to select and which database to contact
+
+    //Used to connect to the Pizza Database and gather the requested information
     public static void connect() {
         Connection conn = null;
+        Statement statement = null;
         try {
+            //Create the database connection
             conn = DriverManager.getConnection(db_url);
-            Statement statement = conn.createStatement();
+            statement = conn.createStatement();
             statement.setQueryTimeout(15);
+            System.out.println("Pizza database connected!\n");
 
-            statement.executeUpdate("SQL STATEMENT GOES HERE");
+            statement.executeUpdate("DROP TABLE IF EXISTS testable");
+            statement.executeUpdate("CREATE TABLE testable (age INTEGER, name VARCHAR(32), PRIMARY KEY (age))");
+            statement.executeUpdate("INSERT INTO testable VALUES (1, 'FIRST')");
+            statement.executeUpdate("INSERT INTO testable VALUES (2, 'SECOND')");
+            statement.executeUpdate("INSERT INTO testable VALUES (3, 'THIRD')");
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM testable");
+            while (rs.next()) {
+                //TODO: Parse database information for requested employee / student
+                System.out.println("ID: " + rs.getInt("age"));
+                System.out.println("ID: " + rs.getString("name"));
+            }
         }
+        //Most likely database isn't found so throw an error
         catch (SQLException e) {
             System.err.println(e.getMessage());
+        } finally {
+            //Close the database connection
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+            //Failure to close connection
+            catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }

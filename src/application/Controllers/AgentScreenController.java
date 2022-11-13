78087536fea25248b6c786e35f7b454c1cf32273
justinozzy <jetoswald@gmail.com>
2022-11-13 13:30:32 -0700

    Added some agent controller functionalities

--- src/application/Controllers/AgentScreenController.java
+++ src/application/Controllers/AgentScreenController.java
@@ -49,31 +49,36 @@ public class AgentScreenController extends SceneController {
         System.out.println("READYLIST: " + PizzaLists.getList("readyList").toString() + "**Added to");
     }
 
-    public void newToRejectedList(){
+    public void newToRejectedList() {
         //Call upon newOrder reject button
-        int count = 0;
         //Iterate through the new list
-        for (Iterator<NodeData> iterator = PizzaLists.getList("newList").iterator(); iterator.hasNext();) {
+        for (Iterator<NodeData> iterator = PizzaLists.getList("newList").iterator(); iterator.hasNext(); ) {
             NodeData curr = iterator.next();
-            //Check if the ids match
-            for (int j = 0; j < IdLists.getIdLists("AgentnewList").size(); j++){
-                //Remove from the Agentnewlist and push node to rejectedList
-                if (curr.getId() == IdLists.getIdLists("AgentnewList").get(j)[0]); {
+            for (int j = 0; j < IdLists.getIdLists("AgentnewList").size(); j++) {
+                //Check if the ids in AgentNewlist and newList match and add them to the readyList
+                if (curr.getId() == IdLists.getIdLists("AgentnewList").get(j)[0]) ;
+                {
                     curr.setStatus(Status.REJECT);
-                    PizzaLists.getList("rejectedList").add(curr);
+                    PizzaLists.getList("readyList").add(curr);
+                    //Remove checked boxes from the pane
+                    int[] cbId = new int[1];
+                    cbId[0] = (IdLists.getIdLists("AgentnewList").get(j)[1]);
+                    IdLists.getIdLists("AgentnewList").remove(j);
+
+                    for (int i = 0; i < AgentNewOrdersVB.getChildren().size(); i++) {
+                        if (Integer.toString(cbId[0]).equals(AgentNewOrdersVB.getChildren().get(i).getId())) {
+                            AgentNewOrdersVB.getChildren().remove(i);
+                        }
+                    }
                     iterator.remove();
-                    //Remove checked boxes from pane
-                    int cbId = IdLists.getIdLists("AgentnewList").get(count)[1];
-                    AgentNewOrdersVB.getChildren().remove(cbId);
+                    System.out.println(curr);
+                    break;
                 }
             }
-            count++;
         }
         System.out.println("REJECTEDLIST: " + PizzaLists.getList("rejectedList").toString() + "**Added to");
         System.out.println("NEWLIST: " + PizzaLists.getList("newList").toString() + "**Removed from");
     }
-
-
 
     public void AgentGetInfo(){
         AgentGetOrdersButton.setDisable(true);
@@ -83,27 +88,32 @@ public class AgentScreenController extends SceneController {
         //Finished Orders
         createCheckBox(PizzaLists.getList("finishedList"), AgentFinishedOrdersVB);
     }
+
     public void finishedListPickedUp(){
         //Call upon finishedOrder [Confirmed Picked-Up] button
         //When a finished order is selected and Picked-Up, order completed, remove it from finished list.
-        int count = 0;
-        for (Iterator<NodeData> iterator = PizzaLists.getList("finishedList").iterator(); iterator.hasNext();) {
+        for (Iterator<NodeData> iterator = PizzaLists.getList("finishedList").iterator(); iterator.hasNext(); ) {
             NodeData curr = iterator.next();
-            for (int j = 0; j < IdLists.getIdLists("AgentfinishedList").size(); j++){
-                if (curr.getId() == IdLists.getIdLists("AgentfinishedList").get(j)[0]) {
-                    iterator.remove();
-                    // Order completed, also search & remove node from allNodesList
-                    for (Iterator<NodeData> allNodesIterator = PizzaLists.getList("allNodesList").iterator(); iterator.hasNext(); ) {
-                        NodeData currAllNodes = allNodesIterator.next();
-                        if (currAllNodes.getId() == IdLists.getIdLists("AgentfinishedList").get(j)[0]) {
-                            allNodesIterator.remove();
-                            int cbId = IdLists.getIdLists("AgentfinishedList").get(count)[1];
-                            AgentFinishedOrdersVB.getChildren().remove(cbId);
+            for (int j = 0; j < IdLists.getIdLists("AgentfinishedList").size(); j++) {
+                //Check if the ids in Agentfinishedlist and finishedList match and add them to the readyList
+                if (curr.getId() == IdLists.getIdLists("AgentfinishedList").get(j)[0]) ;
+                {
+                    PizzaLists.getList("readyList").add(curr);
+                    //Remove checked boxes from the pane
+                    int[] cbId = new int[1];
+                    cbId[0] = (IdLists.getIdLists("AgentfinishedList").get(j)[1]);
+                    IdLists.getIdLists("AgentfinishedList").remove(j);
+
+                    for (int i = 0; i < AgentFinishedOrdersVB.getChildren().size(); i++) {
+                        if (Integer.toString(cbId[0]).equals(AgentFinishedOrdersVB.getChildren().get(i).getId())) {
+                            AgentNewOrdersVB.getChildren().remove(i);
                         }
                     }
+                    iterator.remove();
+                    System.out.println(curr);
+                    break;
                 }
             }
-            count++;
         }
         System.out.println("FINISHEDLIST: " + PizzaLists.getList("finishedList").toString());
     }
